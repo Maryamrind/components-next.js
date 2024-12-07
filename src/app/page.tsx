5f57dd611c4829baab8a1a2723324ead604421e6
Maryamrind <maryamanwer75@gmail.com>
2024-12-07 12:03:44 -0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// import Header from "./components/header";
+// import About from "./components/about";
+
+// export default function Home (){
+//   return(
+//     <div className="home">
+//       <Header></Header>
+//       <About></About>
+//       <h1>
+//         Home Page
+//       </h1>
+
+//     </div>
+//   )
+// };
